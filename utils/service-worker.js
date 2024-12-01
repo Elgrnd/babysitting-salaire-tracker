f444eb81fe5c2ba46fc293062411579448816981
Galdric Desert-Caparros <galdric.desert-caparros@etu.umontpellier.fr>
2024-12-01 11:28:31 +0100

    test pwa

--- utils/service-worker.js
+++ utils/service-worker.js
@@ -33,7 +33,7 @@ self.addEventListener("activate", (event) => {
 // Interception des requêtes
 self.addEventListener("fetch", (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
+        caches.match(event.request).then(response => {
             return response || fetch(event.request);
         })
     );

d8db5244aa7acda72927a66c465f882179f064bd
Galdric Desert-Caparros <galdric.desert-caparros@etu.umontpellier.fr>
2024-12-02 22:26:57 +0100

    test pwa

--- utils/service-worker.js
+++ utils/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pwa-cache-v000001111122222222223333333";
+const CACHE_NAME = "pwa-cache-v000001111122222222223333333444444444444";
 const urlsToCache = [
     "/",
     "/ressources/css/styles.css",
